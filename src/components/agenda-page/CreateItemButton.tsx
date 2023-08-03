2fd4de9d43b47e655147163de6993b5c2d1abb48
Shakib Rahman <cakoshakib@gmail.com>
2023-08-02 22:31:51 -0400

    css expert

--- src/components/agenda-page/CreateItemButton.tsx
+++ src/components/agenda-page/CreateItemButton.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { Button, Typography } from "antd";
+import { Button, Typography, Card } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type CreateItemButtonProps = {
@@ -9,11 +9,9 @@ type CreateItemButtonProps = {
 
 const CreateItemButton: React.FC<CreateItemButtonProps> = ({ createItemAction }) => {
   return (
-    <Button onClick={createItemAction} className="px-2">
-      <div className="flex justify-between gap-3 items-center">
+    <Button onClick={createItemAction} className="px-2 flex justify-between gap-2 items-center">
         <FontAwesomeIcon icon={faPlus} />
         <Typography.Text>Create item</Typography.Text>
-      </div>
     </Button>
   );
 };
